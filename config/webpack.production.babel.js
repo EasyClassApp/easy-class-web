217c287241d01972c9d2844c1de01431258bad21
Rafael Companhoni <companhoni@gmail.com>
2018-10-29 21:06:32 -0300

    Configurações para deploy com Travis e Heroku

--- config/webpack.production.babel.js
+++ config/webpack.production.babel.js
@@ -2,17 +2,22 @@ import merge from 'webpack-merge';
 import commonConfig from './webpack.common.babel';
 import * as parts from './webpack.parts.babel';
 
-const productionConfig = merge([
-  commonConfig,
-  parts.imagesLoader({
-    options: {
-      limit: 8192,
-      name: './assets/[name].[ext]',
-    },
-  }),
-  parts.minificationPlugin(),
-  parts.setFreeVariablePlugin('process.env.NODE_ENV', 'production'),
-  parts.setFreeVariablePlugin('SERVER_URL', 'https://easy-class-api-production.herokuapp.com'),
-]);
+export default (env) => {
+  const SERVER_URL = env.NODE_ENV === 'production' ? 'https://easy-class-api.herokuapp.com' : 'https://easy-class-api-staging.herokuapp.com';
 
-export default productionConfig;
+  const productionConfig = merge([
+    commonConfig,
+    parts.imagesLoader({
+      options: {
+        limit: 8192,
+        name: './assets/[name].[ext]',
+      },
+    }),
+    parts.minificationPlugin(),
+    parts.setFreeVariablePlugin('process.env.NODE_ENV', 'production'),
+    parts.setFreeVariablePlugin('SERVER_URL', SERVER_URL),
+  ]);
+
+  console.log('THIS APP WILL CONSUME THE API AT', SERVER_URL);
+  return productionConfig;
+};
